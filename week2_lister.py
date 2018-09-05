2396b973962f15e8fd951a59b652ae960ef7614e
Unknown <erikfsk@student.matnat.uio.no>
2018-09-05 15:48:37 +0200

    litt mer mer hjelp

--- week2_lister.py
+++ week2_lister.py
@@ -43,26 +43,26 @@ print(t1_t2_listen_listen[0][0][2],t1_t2_listen_listen[0][1][2])
 
 
 
-# t = [i for i in range(11)]
-# y = [i*i for i in t]
+t = [i for i in range(11)]
+y = [i*i for i in t]
 
 
-# ty1 = [t,y]
-# ty2 = [[t_i,y_i] for t_i,y_i in zip(t,y)]
+ty1 = [t,y]
+ty2 = [[t_i,y_i] for t_i,y_i in zip(t,y)]
 
 
-# print( "t",t)
-# print( "y",y)
-# print( "ty1",ty1)
-# print( "ty2",ty2)
+print( "t",t)
+print( "y",y)
+print( "ty1",ty1)
+print( "ty2",ty2)
 
 
-# for i in range(len(ty1[0])):
-# 	print(ty1[0][i],ty1[1][i])
+for i in range(len(ty1[0])):
+	print(ty1[0][i],ty1[1][i])
 
 
-# for i in range(len(ty2)):
-# 	print(ty2[i][0],ty2[i][1])
+for i in range(len(ty2)):
+	print(ty2[i][0],ty2[i][1])
 
 
 
