f77c78e77e424f238fb41edb17c2841ea45af613
Unknown <erikfsk@student.matnat.uio.no>
2018-11-13 15:35:09 +0100

    test

--- test9.py
+++ test9.py
@@ -5,12 +5,12 @@ from matplotlib.pyplot import *
 def f(t,x):
 	return (-x*sin(t))+sin(t)
 
-def eulers(t_k1,x_k,h):
-	return x[-1]+h*f(t_i-h,x[-1])
+def eulers(t_k,x_k,h):
+	return x_k+h*f(t_k,x[-1])
 
-def eulers_midpoint(t_k1,x_k,h):
-	x_midpoint_value = x_k+0.5*h*f(t_k1-h,x_k)
-	return x_k+h*f(t_k1-0.5*h,x_midpoint_value)
+def eulers_midpoint(t_k,x_k,h):
+	x_midpoint_value = x_k+0.5*h*f(t_k,x_k)
+	return x_k+h*f(t_k+0.5*h,x_midpoint_value)
 
 for n in [1,2,5,10]:
 	#t values
@@ -21,16 +21,19 @@ for n in [1,2,5,10]:
 	
 	#starting solution lists
 	x_midt = [x0];x = [x0];
-	for t_i in t[1::]:
+	for t_k in t[:-1:]:
 		#euler
-		x.append(eulers(x[-1],t_i,h))
+		x.append(eulers(t_k,x[-1],h))
 		#eulers midtpunkt
-		x_midt.append(eulers_midpoint(t_i,x_midt[-1],h))
+		x_midt.append(eulers_midpoint(t_k,x_midt[-1],h))
 
 	#plot both results
+	print("-"*40)
 	plot(t,x,label="n=%d"%n)
+	print("%-15s n=%-10d f_eval=%-10d" % ("euler",n,n))
 	if n == 1 or n == 5:
 		plot(t,x_midt,label="n=%d midpoint"%n)
+		print("%-15s n=%-10d f_eval=%-10d" % ("midpoint",n,2*n))
 
 legend()
 ylim(1,5)
