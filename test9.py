68048698a9460cab1a531d76ab3a53d796a92f01
Unknown <erikfsk@student.matnat.uio.no>
2018-11-13 15:43:57 +0100

    veldig bra, nesten

--- test9.py
+++ test9.py
@@ -12,7 +12,7 @@ def eulers_midpoint(t_k,x_k,h):
 	x_midpoint_value = x_k+0.5*h*f(t_k,x_k)
 	return x_k+h*f(t_k+0.5*h,x_midpoint_value)
 
-for n in [1,2,5,10]:
+for n in [1,2,5,10,100]:
 	#t values
 	t = linspace(0,2*pi,n+1)
 	
@@ -27,14 +27,21 @@ for n in [1,2,5,10]:
 		#eulers midtpunkt
 		x_midt.append(eulers_midpoint(t_k,x_midt[-1],h))
 
-	#plot both results
-	print("-"*40)
-	plot(t,x,label="n=%d"%n)
-	print("%-15s n=%-10d f_eval=%-10d" % ("euler",n,n))
+	
 	if n == 1 or n == 5:
+		print("-"*40)
+		plot(t,x,label="n=%d"%n)
+		print("%-15s n=%-10d f_eval=%-10d" % ("euler",n,n))
 		plot(t,x_midt,label="n=%d midpoint"%n)
 		print("%-15s n=%-10d f_eval=%-10d" % ("midpoint",n,2*n))
+	elif n == 100:
+		plot(t,x_midt,label="n=%d midpoint"%n)
+	else:
+		print("-"*40)
+		plot(t,x,label="n=%d"%n)
+		print("%-15s n=%-10d f_eval=%-10d" % ("euler",n,n))
 
+print("-"*40)
 legend()
 ylim(1,5)
 show()
